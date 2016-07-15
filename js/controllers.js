0d0a50f74f6851612b66e41d9304bac4badffa62
Jose Raul Alfaro Guillen <jralfarog@gmail.com>
2016-07-15 09:46:09 -0600

    ásd

---
+++ js/controllers.js
@@ -0,0 +1,92 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+})
+
+
+.controller('loginCtrl', function($scope, $stateParams, login) {
+$scope.loginData={};
+  $scope.doLogin = function (){
+   // var user = $scope.loginData.username;
+   // console.log(user);
+  login.login();
+  //  console.log('loginnnn');
+  }
+})
+
+.factory('login', function($http, $q) {
+  return {
+    login: function() {
+
+  var deferred = $q.defer();
+      $http({
+    method: 'POST',
+    url: 'https://solinte.net/OAuth2/Token',
+    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+    transformRequest: function(obj) {
+        var str = [];
+        for(var p in obj)
+        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+        return str.join("&");
+    },
+    data: {grant_type:'password',client_id:'solinteasyeapp',client_secret:'plmoknijbuhvygctfxrdzesawq',username:'demo10@solinte.net',password:'demo', scope:'perfil admin_rol'}
+}).success(function (response, status) {console.log(response);
+  deferred.resolve(response);});
+    return deferred.promise;
+    }
+  }
+});
+
+
